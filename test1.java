553380b6752884ae37263d3ce33d5a55baa2175f
Mike Gittelsohn <gittel@twocats.com>
2017-03-14 14:09:16 -0700

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,7 +1,7 @@
 public test1 {
 
-public static void main (String[] args) {
-  System.out.println ("hello");
-}
+  public static void main (String[] args) {
+    System.out.println ("hello");
+  }
 
 }
